54de6be552d31a1599995b23d2a656b11ec918fd
Amadeus <haris_owais@hotmail.com>
2023-06-11 15:46:58 +0500

    Added ChatEngine component to App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 import "./App.css";
+
+import { ChatEngine } from "react-chat-engine";
+
+const App = () => {
+  return(
+    <ChatEngine
+      height='100vh'
+      projectID=''
+      userName=''
+      userSecret=''
+    />
+  )
+}
+
+export default App;
